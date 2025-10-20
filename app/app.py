253ca367ebce2fd81535e1eacd989e5c8826c880
Müni <lunalith.xyz@proton.me>
2025-10-20 16:55:39 +0000

    Add New Relic APM monitoring integration

--- app/app.py
+++ app/app.py
@@ -1,5 +1,10 @@
-from flask import Flask
 import os
+
+if os.getenv("NEW_RELIC_LICENSE_KEY"):
+    import newrelic.agent
+    newrelic.agent.initialize()
+
+from flask import Flask
 
 app = Flask(__name__)
 
