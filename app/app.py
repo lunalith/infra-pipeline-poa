75b5780f5a1544298fe67bdb1c9a047a9dd22f41
Müni <lunalith.xyz@proton.me>
2025-10-20 16:55:39 +0000

    Refactor port configuration to use inline os.getenv

--- app/app.py
+++ app/app.py
@@ -21,5 +21,4 @@ def health():
     return {"status": "ok"}, 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
